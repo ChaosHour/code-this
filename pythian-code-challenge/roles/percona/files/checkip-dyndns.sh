6a14930ad540f50fa6eaf7b0e31783732224743d
chaoshour <kurt_lv@cox.net>
2016-08-13 12:21:20 -0700

    cleaned up checkip-dyndns.sh header

--- pythian-code-challenge/roles/percona/files/checkip-dyndns.sh
+++ pythian-code-challenge/roles/percona/files/checkip-dyndns.sh
@@ -2,6 +2,7 @@
 ################################################
 # Script to output the datetime, with public ip
 # Created for the purpose of this code challenge
+# Uncomment set to debug this script.
 # 07/18/2016
 # Created By Kurt Larsen
 ################################################
